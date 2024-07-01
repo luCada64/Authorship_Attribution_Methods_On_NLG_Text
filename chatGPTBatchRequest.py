086be49c86e415f415f197a14616c60d0299cd8f
Lyka Ada Rhodes <lucabenrhodes@gmail.com>
2024-07-01 10:45:26 +0200

    Uploading Code

---
+++ chatGPTBatchRequest.py
@@ -0,0 +1,4 @@
+# This code will take a json file of abstract authors, titles and pulication year
+import json
+import pandas as pd
+
--- chatGPTtest.py
+++ chatGPTtest.py
@@ -10,13 +10,12 @@ from chatgptApiKey import OPENAI_API_KEY
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 
-# Let's access the clients
-completion = client.chat.completions.create(
+response = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
   ]
 )
-
-print(completion.choices[0].message)
