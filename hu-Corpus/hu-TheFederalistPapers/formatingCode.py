30f997e554673f2bd9e77d7d4529565a97ae5a3f
Lyka Ada Rhodes <lucabenrhodes@gmail.com>
2024-07-15 15:39:01 +0200

    Attempted to get the formaing correct

--- hu-Corpus/hu-TheFederalistPapers/formatingCode.py
+++ hu-Corpus/hu-TheFederalistPapers/formatingCode.py
@@ -12,19 +12,58 @@ with open("hu-Corpus/hu-TheFederalistPapers/complete-Federalist-Paper.txt", mode
     completePapers_lines = completePapers.split('\n')
 
     
-    # Skip until the program find the first paper
+    # # Skip until the program find the first paper
+    # index = 0
+    # while completePapers_lines[index] != "FEDERALIST No. 1":
+        
+    #     # Let find the index
+    #     index = index+1
+        
+    
+    # Remove the unseary lines
+    completePapers_lines = completePapers_lines[37:]
+    
+        
+    currentPaper = []
+    
     index = 0
-    while completePapers_lines[index] != "FEDERALIST No. 1":
-        
-        # Let find the index
-        index = index+1
-        
-        
-        
+    current_author = ""
+    currrent_title = ""
+    current_year = 0000
+    
+    # temp 
+    completePapers_lines = completePapers_lines[:500]
+    
     for line in completePapers_lines:
         
-        # Add a break clause
-        if line[:13] == "FEDERALIST No.":
-            print(line)
         
+        
+        # Add a break clause
+        if line[:14] != "FEDERALIST No.":
+            index = index +1
+            
+        
+        if line[:14] == "FEDERALIST No.":
+            
+    
+            print("\n\n\n"+line+"\n")
+            
+            # Lets get out the old text
+            previous_text = completePapers_lines[:index-1]
+            
+            # save the data
+            print(previous_text)
+            
+            
+            
+            # Update complete Paper to remove all that come befroe
+            completePapers_lines = completePapers_lines[index:]
+            
+            paperNum = line
+            title = completePapers_lines
+            
+            print(completePapers_lines)
+            
+        
+
         
