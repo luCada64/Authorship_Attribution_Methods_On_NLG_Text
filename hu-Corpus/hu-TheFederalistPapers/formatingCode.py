54efb0f10fc5ed96700dabc3c36c2d5366400079
Lyka Ada Rhodes <lucabenrhodes@gmail.com>
2024-07-17 23:56:01 +0200

    Worked on saving code

--- hu-Corpus/hu-TheFederalistPapers/formatingCode.py
+++ hu-Corpus/hu-TheFederalistPapers/formatingCode.py
@@ -7,11 +7,16 @@ def saveFile(title, author, year, text):
     author = ''.join(letter for letter in author if letter.isalpha())
     title = ''.join(letter for letter in title.title() if letter.isalpha())
     
+
+    print(author)
+    print(title)
     
-    file = "hu---{}---{}---{}".format(author, title, str(year))
+
+
+    file = "hu---{}---{}---{}.txt".format(author, title, str(year))
+    path = "hu-Corpus/hu-TheFederalistPapers/"
     
-    
-    with open( file , "w") as savefile:
+    with open( path+file , "w") as savefile:
     
             text = "\n".join(text)
     
@@ -42,49 +47,60 @@ with open("hu-Corpus/hu-TheFederalistPapers/complete-Federalist-Paper.txt", mode
         
     
     # Remove the unseary lines
-    completePapers_lines = completePapers_lines[37:]
+    completePapers_lines = completePapers_lines[36:]
     
         
-    currentPaper = []
     
     index = 0
     paperNum = ""
-    current_author = ""
-    currrent_title = ""
-    current_year = 0000
+    author = ""
+    title = ""
+    year = "0000"
     
     # temp 
-    completePapers_lines = completePapers_lines[:500]
+    completePapers_lines = completePapers_lines[:1000]
     
     for line in completePapers_lines:
         
+        # Stop the code if when you run out of lines  
         if line[:3] == "***":
             break
         
         
-        # Add a break clause
-        if line[:14] != "FEDERALIST No.":
-            index = index +1
-            
-        
+        # Get the current title
         if line[:14] == "FEDERALIST No.":
-            
-    
-            # Lets get out the old text
-            previous_text = completePapers_lines[:index-1]
-            
-            # save the data
-            saveFile(currrent_title, current_author, current_year, previous_text)
-            
-            
-            
-            # Update complete Paper to remove all that come befroe
-            completePapers_lines = completePapers_lines[index:]
-            
+            # Extract metaa data
             paperNum = line
-            title = completePapers_lines
             
-            print(completePapers_lines)
+            title = completePapers_lines[2]
+            
+            year = completePapers_lines[4][-4:]
+            
+            author = completePapers_lines[7]
+            
+            index = index + 7
+            
+            text = []
+            
+            # Let's Extract the content
+            while completePapers_lines[index][:14] != "FEDERALIST No.":
+            
+                print(completePapers_lines[index])
+            
+                text.append(completePapers_lines[index])
+                
+                index = index + 1
+                
+            # Now that we got all the conent we may save it
+            saveFile(title, author, year, text )
+            
+            
+            # Now we need to remove all the lines up to that point
+            
+            
+            
+            exit()
+
             
         
 
