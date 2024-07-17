d8aea276f57f15eef98cb1ee9461fe7bc8068235
Lyka Ada Rhodes <lucabenrhodes@gmail.com>
2024-07-18 00:21:03 +0200

    Fixed Consiteny with complet Federalist paper

--- hu-Corpus/hu-TheFederalistPapers/formatingCode.py
+++ hu-Corpus/hu-TheFederalistPapers/formatingCode.py
@@ -1,16 +1,10 @@
 # This code will take the complete Federalist paper txt file and extract out
 # The title, the author and year for each paper
 
+# An assumption we are making is that all paper credide as "Madison with Hamiloton" as just madison for consitency
+
 
 def saveFile(title, author, year, text):
-    
-    author = ''.join(letter for letter in author if letter.isalpha())
-    title = ''.join(letter for letter in title.title() if letter.isalpha())
-    
-
-    print(author)
-    print(title)
-    
 
 
     file = "hu---{}---{}---{}.txt".format(author, title, str(year))
@@ -50,7 +44,7 @@ with open("hu-Corpus/hu-TheFederalistPapers/complete-Federalist-Paper.txt", mode
     year = "0000"
     
     # temp 
-    completePapers_lines = completePapers_lines[:1000]
+    completePapers_lines = completePapers_lines
     
     for line in completePapers_lines:
         
@@ -68,32 +62,39 @@ with open("hu-Corpus/hu-TheFederalistPapers/complete-Federalist-Paper.txt", mode
             
             year = completePapers_lines[4][-4:]
             
-            author = completePapers_lines[7]
+            author = completePapers_lines[6]
             
             index = index + 7
+            
+            # Comidn paper number into tittle
+            title = "{}: {}".format(paperNum, title)
             
             text = []
             
             # Let's Extract the content
             while completePapers_lines[index][:14] != "FEDERALIST No.":
             
-                print(completePapers_lines[index])
-            
                 text.append(completePapers_lines[index])
                 
                 index = index + 1
+                
+            
+        
+            # Changed the character to fixe fomat
+            author = ''.join(letter for letter in author if letter.isalpha())
+            title = ''.join(letter for letter in title.title() if letter.isalpha() or letter.isdigit())
+    
+        
         
             # Now that we got all the conent we may save it
-            saveFile(paperNum+title, author, year, text )
+            saveFile(title, author, year, text )
             
-            
+    
             # Now we need to remove all the lines up to that point
-            
-            
+            completePapers_lines = completePapers_lines[index:]
             
             exit()
 
-            
         
 
         
