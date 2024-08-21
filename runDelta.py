46ab96571a866710e3a392469992b7eaa99e96d1
Lyka Ada Rhodes <lucabenrhodes@gmail.com>
2024-08-21 11:59:04 +0100

    Updatde gitignore

--- runDelta.py
+++ runDelta.py
@@ -39,6 +39,19 @@ def camelCaseToNormal(string):
 
     return outputString
 
+def getAuthorFromTitle(authorAndTitle):
+    
+    author = ""
+    # Get the Author
+    for letter in authorAndTitle:
+        if letter == "-": break
+
+        author = author + letter
+    
+    return author[1:-1]
+    
+
+
 
 # Load the text into corpus:
 
@@ -98,7 +111,6 @@ ai_corpus.tokenise(tokenise_remove_pronouns_en)
 # We used the AI, traing data as our test data. 
 probatlities = calculate_burrows_delta(hu_corpus, ai_corpus)
 
-print(probatlities)
 
 # We can save the data as csv so that it can be view later
 probatlities.to_csv(outputDataPath+"spreadsheet.csv")
@@ -107,49 +119,74 @@ probatlities.to_csv(outputDataPath+"spreadsheet.csv")
 # To avoid this were going to condence the data.
 # Instead of showing each indivdual text were going to use who the AI was impersentaing 
 
-# So the way were going to do this is a bit silly, because the data is in altthecical order we will use a loop
 
-humanReadableProbablity = probatlities
-
-i = 0
+authors = []
 previousAuthor = ""
 
+
+# We need to get every author and calculate a tally
 for authorAndTitle in probatlities.head():
     
-    author = ""
-    # Get the Author
-    for letter in authorAndTitle:
-        if letter == "-": break
-
-        author = author + letter
-    
-    author = author[1:-1]
+    author = getAuthorFromTitle(authorAndTitle)
     
     # Check if the author is the same the previous one
     # We can add 
     if previousAuthor == "": 
         previousAuthor = author
-    
-    
-    if author == previousAuthor:
-        i = i + 1
-    else:
-        print(previousAuthor+": " + str(i))
-        i = 0
+        authors.append(previousAuthor)
         
+    if author != previousAuthor:
+        authors.append(author)
         previousAuthor = author
+        
+    
+# First were going to transpose the data frame
+trans_probablities = probatlities.transpose()
 
-print(previousAuthor+": " + str(i))
+
+
+# print(trans_probablities)
+trans_probablities.insert(0, "Title", trans_probablities.index)
+trans_probablities.reset_index()
+
+
+# Now lets extract the names, and give it a seperat coloum
+names = trans_probablities["Title"].apply(lambda title: getAuthorFromTitle(title))
+trans_probablities.insert(1, "Author", names)
+
+
+
+avr_prob = pd.DataFrame(index=authors, columns=authors, dtype="float64")
 
 
 
 
-print(humanReadableProbablity)
+# Now we can avreage out the probalities where the author is the same.
+for author in authors:
+    author_data = trans_probablities[trans_probablities["Author"] == author]
+    
+    # Now for each author we must calculat the mean for every author
+    for j_author in authors:
+        avr_prob[author][j_author] = author_data[" " + j_author].mean()
+        
+    # print(author_data[" Alexander Hamilton"].mean())
+
+avr_prob.to_csv(outputDataPath+"avr_prob.csv")
+
 
 
 # Visualise
 
 # Were going to use seaborn as our visulsation data
 
-# sns.heatmap(probatlities, cmap="YlGnBu", annot=True, fmt="0.0f")
-# plt.savefig(outputDataPath+"AllData.png", dpi="700")
+
+plot = sns.heatmap(avr_prob, annot=avr_prob, cmap="YlGnBu" ) 
+
+plot.set_title("Burros Delta Similarity Score (lower = more similar)", pad=20)
+plot.set_xlabel("Actual Human Author", labelpad=5)
+plot.set_ylabel("Delta Score, for impersonated author",labelpad=10)
+
+
+
+# Save the figers
+plt.savefig(outputDataPath+"burrows_mean_probaltities_svg", format="svg" )
